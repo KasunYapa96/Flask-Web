31d559879846ba6c7293e7d1fef5039be19032df
KasunYapa96 <kasunishankayapa1996@gmail.com>
2020-11-16 13:59:00 +0530

    render_html

--- app2.py
+++ app2.py
@@ -6,17 +6,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     # return render_template('index.html')
-    return "Hello World"
-
-
-@app.route('/<name>')
-def user(name):
-    return f"Hello {name}!"
-
-
-@app.route('/admin')
-def index():
-    return redirect(url_for("home"))
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
